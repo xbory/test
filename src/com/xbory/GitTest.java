097c67699cb6b4312a3b10d76e8730d75ba6dbea
xbory <xbory@nciae.com>
2022-12-12 11:17:00 +0800

    xbory  3commit

--- src/com/xbory/GitTest.java
+++ src/com/xbory/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git111");
         System.out.println("git222");
+        System.out.println("git333");
     }
 }
