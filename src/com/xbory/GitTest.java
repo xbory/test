5ced33385981b796cddb474f0edb4ad446694964
xbory <xbory@nciae.com>
2022-12-13 10:48:15 +0800

    master 2 commit

--- src/com/xbory/GitTest.java
+++ src/com/xbory/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hotfix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("master test");
     }
 }
