d8b925923ca18782c0d3bcb7b8cc0f22f65ab3a9
gyh <2039343942@qq.com>
2022-12-13 18:11:08 +0800

    first commit

--- src/com/xbory/G.java
+++ src/com/xbory/G.java
@@ -2,6 +2,10 @@ package com.xbory;
 
 public class G {
     public static void main(String[] args) {
-
+        System.out.println("kdsjf");
+        System.out.println("kdsjf");
+        System.out.println("kdsjf");
+        System.out.println("kdsjf");
+        System.out.println("kes");
     }
 }
